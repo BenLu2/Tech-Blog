af23b95254a6989430da63776405530593435768
Ben Lu <yudonglv@gmail.com>
2022-02-19 23:32:45 -0800

    building initial structure

---
+++ tom.js
@@ -0,0 +1,13 @@
+function phoneNUmberClean(num) {
+    console.log('before sub string', num);
+    var cleanNum = num.substring(1);
+    console.log('after sub string',cleanNum)
+}
+
+phoneNUmberClean('1-555-555-5555');
+
+
+// 1 per day!!
+// 7 gui's in react!
+// react memory game tutorial , tutorials in general
+// algos in leet code
