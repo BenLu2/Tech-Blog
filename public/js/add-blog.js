b5b74ccbfcddec2309532e15cbe1a1348e938528
Ben Lu <yudonglv@gmail.com>
2022-03-16 15:29:19 -0700

    m

--- public/js/add-blog.js
+++ public/js/add-blog.js
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-$("#newPost-btn").on("click", function(e) {
-    e.preventDefault();
-    let newPost = {
-        title: $("#newPost-title").val(),
-        body: $("#newPost-body").val()
-    }
-    console.log("new post data", newPost)
-    fetch("/api/save", {
-        method: "POST",
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(newPost)
-    }).then(function(response) {
-        return response.json()
-    }).then(function(data) {
-        console.log("Data from backend after the fetch", data)
-        window.location.reload()
-    })
-})
-
-$("#newComment-btn").on("click", function(e) {
-    e.preventDefault();
-    let newComment = {
-        body: $("#newComment-body").val()
-    }
-    console.log("new comment data", newComment)
-    fetch("/api/savecomment", {
-        method: "POST",
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(newComment)
-    }).then(function(response) {
-        return response.json()
-    }).then(function(data) {
-        console.log("Data from backend after the fetch", data)
-    })
-})
-=======
 var newBlogForm = async(e) => {
   e.preventDefault();
 
@@ -71,4 +28,3 @@ var newBlogForm = async(e) => {
 document.querySelector('#new-blog-form').addEventListener('submit', newBlogForm);
 
 console.log(document.querySelector('#new-blog-form'))
->>>>>>> parent of 569eb7d (changes)
